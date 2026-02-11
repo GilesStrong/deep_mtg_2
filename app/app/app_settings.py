f032264c07eb2f4f286f8063b9672eeff659f43f
GilesStrong <giles.strong@outlook.com>
2026-02-11 04:16:43 +0000

    feat: app settings

---
+++ app/app/app_settings.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from typing import Literal
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+type ENV = Literal['development', 'staging', 'production']
+
+
+class EnvSettings(BaseSettings):
+    DEBUG: bool
+    ENVIRONMENT: ENV
+
+
+class DjangoSettings(BaseSettings):
+    SECRET_KEY: str
+    DEBUG: bool
+    ALLOWED_HOSTS: list[str]
+
+
+class CelerySettings(BaseSettings):
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
+    CELERY_TASK_DEFAULT_QUEUE: str
+    CELERY_TASK_DEFAULT_EXCHANGE: str
+    CELERY_TASK_DEFAULT_ROUTING_KEY: str
+    CELERY_TASK_CREATE_MISSING_QUEUES: bool
+
+
+class PostgresSettings(BaseSettings):
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+
+class AISettings(BaseSettings):
+    OLLAMA_BASE_URL: str
+
+
+class AppSettings(EnvSettings, DjangoSettings, CelerySettings, PostgresSettings, AISettings):
+    model_config = SettingsConfigDict(env_file_encoding='utf-8')
+
+
+def find_env_file() -> Path | None:
+    current_dir = BASE_DIR
+    for _ in range(5):
+        env_file_path = current_dir / '.env'
+        if env_file_path.is_file():
+            return env_file_path
+        current_dir = current_dir.parent
+    return None
+
+
+def get_app_settings() -> AppSettings:
+    env_file_path = find_env_file()
+    if env_file_path:
+        return AppSettings(_env_file=env_file_path)  # type: ignore[call-arg]
+    else:
+        raise RuntimeError('.env file not found')
+
+
+APP_SETTINGS = get_app_settings()
--- app/app/settings.py
+++ app/app/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 from kombu import Exchange, Queue
+
+from app.app_settings import APP_SETTINGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uj_s#k1xgb(#^z#67w-ziz*#wb!3%z&p)u4glzv=!ois*qhprt'
+SECRET_KEY = APP_SETTINGS.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = APP_SETTINGS.DEBUG
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = APP_SETTINGS.ALLOWED_HOSTS
 
 
 # Application definition
@@ -127,13 +128,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379/1")
+CELERY_BROKER_URL = APP_SETTINGS.CELERY_BROKER_URL
+CELERY_RESULT_BACKEND = APP_SETTINGS.CELERY_RESULT_BACKEND
 
-CELERY_TASK_DEFAULT_QUEUE = "default"
-CELERY_TASK_DEFAULT_EXCHANGE = "default"
-CELERY_TASK_DEFAULT_ROUTING_KEY = "default"
-CELERY_TASK_CREATE_MISSING_QUEUES = False
+CELERY_TASK_DEFAULT_QUEUE = APP_SETTINGS.CELERY_TASK_DEFAULT_QUEUE
+CELERY_TASK_DEFAULT_EXCHANGE = APP_SETTINGS.CELERY_TASK_DEFAULT_EXCHANGE
+CELERY_TASK_DEFAULT_ROUTING_KEY = APP_SETTINGS.CELERY_TASK_DEFAULT_ROUTING_KEY
+CELERY_TASK_CREATE_MISSING_QUEUES = APP_SETTINGS.CELERY_TASK_CREATE_MISSING_QUEUES
 
 CELERY_TASK_QUEUES = (
     Queue("default", Exchange("default"), routing_key="default"),
