5bc77a96cd929b675adf4f266842017d538975ae
GilesStrong <giles.strong@outlook.com>
2026-02-27 12:09:46 +0000

    fix: warnings and errors

---
+++ app/appai/constants/guardrail_contexts.py
@@ -0,0 +1,4 @@
+BUILD_DECK_CONTEXT = """
+The user should be request to build a Magic: The Gathering deck, or to refine an exisitng deck.
+The request may be provided in a fun, or creative way, and provided it a a relevant build request, this alone is not ground for removal.
+"""
